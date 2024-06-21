23a659ab0d5a25fe674973e3225d0e779576a783
Zain2983 <zainijaz2983@gmail.com>
2024-06-21 16:26:55 +0500

    front end complete

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"; 
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import NoteCard from "../../components/Cards/NoteCard";
 import AddEditNotes from "./AddEditNotes";
@@ -64,7 +64,7 @@ const Home = () => {
       <button
         className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10 "
         onClick={() => {
-          setOpenAddEditModal({isShown:true,type:"add",data:null});
+          setOpenAddEditModal({ isShown: true, type: "add", data: null });
         }}
       >
         <MdAdd className="text-[32px] text-white" />
@@ -81,7 +81,13 @@ const Home = () => {
         contentlabel=""
         className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow"
       >
-        <AddEditNotes />
+        <AddEditNotes
+        type={openAddEditModal.type}
+        noteData={openAddEditModal.data}
+          onClose={() => {
+            setOpenAddEditModal({ isShown: false, type: "add", data: null });
+          }}
+        />
       </Modal>
     </>
   );
