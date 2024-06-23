efa75d7b43ee78f396eba49aa7a455d301dd7d3f
Zain2983 <zainijaz2983@gmail.com>
2024-06-24 03:46:55 +0500

    made the add-note API fully functional

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,14 @@ const mongoose = require("mongoose");
 mongoose.connect(config.connectionString);
 
 const User = require("./models/user.model");
+const Note = require("./models/note.model");
 
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
 const jwt = require("jsonwebtoken");
-const { autheticateToken } = require("./utilities");
+const { authenticateToken } = require("./utilities");
 
 app.use(express.json());
 
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
   res.json({ data: "hello" });
 });
 
+// Create Account
 app.post("/create-account", async (req, res) => {
   const { fullName, email, password } = req.body;
 
@@ -70,10 +72,83 @@ app.post("/create-account", async (req, res) => {
   });
 });
 
+// Login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const userInfo = await User.findOne({ email: email });
+  if (!userInfo) {
+    res.status(400).json({ message: "User not found" });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "36000m",
+    });
+
+    return res.json({
+      error: false,
+      message: "Login Successful",
+      email,
+      accessToken,
+    });
+  } else {
+    return res
+      .status(400)
+      .json({ error: true, message: "Invalid Credentials" });
+  }
+});
+
+app.post("/add-note", authenticateToken, async (req, res) => {
+  const { title, content, tags } = req.body;
+  const { user } = req.user;
+
+  if (!title) {
+    return res.status(400).json({ error: true, message: "Title is required" });
+  }
+
+  if (!content) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Content is required" });
+  }
+
+  try {
+    const note = new Note({
+      title,
+      content,
+      tags: tags || [],
+      userId: user._id,
+    });
+
+    await note.save();
+
+    return res.json({
+      error: false,
+      note,
+      message: "Note added successfully",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Internal Server Error" });
+  }
+});
+
 app.listen(8000);
 
 module.exports = app;
 
 // https://www.youtube.com/watch?v=Rgvec9UA2_I
-// 1:13:57
-// 1:17:45
+// 1:27:56
